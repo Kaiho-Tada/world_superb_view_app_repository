893f8f9b85cfacec2a8884723876fe4b2f343e55
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2023-10-22 20:17:27 +0900

    userのtypeの不要なプロパティを削除

--- frontend/app/src/types/api/user.ts
+++ frontend/app/src/types/api/user.ts
@@ -1,10 +1,6 @@
 export type User = {
   id: number;
   email: string;
-  uid: string;
-  provider: string;
   name: string;
-  nickname?: string;
-  image?: string;
-  allowPasswordChange: boolean;
+  nickname: string;
 };
