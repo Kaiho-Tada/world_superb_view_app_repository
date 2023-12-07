eb2e498974f78e56a5a939d4395bdef1b3a82428
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2023-12-07 22:16:47 +0900

    RiskLevelStar.test.tsxの不要なdebugを削除

--- frontend/app/src/test/unit/components/RiskLevelStar.test.tsx
+++ frontend/app/src/test/unit/components/RiskLevelStar.test.tsx
@@ -2,8 +2,7 @@ import { render, screen } from "@testing-library/react";
 import RiskLevelStar from "components/molecules/RiskLevelStar";
 
 test("propsで渡されたriskLevelと同じ数の星マークが表示されていること", () => {
-  const { debug } = render(<RiskLevelStar maxRiskLevel={3} />);
-  debug();
+  render(<RiskLevelStar maxRiskLevel={3} />);
   const riskLevel = screen.getAllByRole("img", { name: "リスクレベル" });
   expect(riskLevel.length).toBe(3);
 });
