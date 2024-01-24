4db5da5f4ca2bf259b5041b6969f4c277f0cac5e
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-01-24 15:32:01 +0900

    rescueのインデントを修正

--- backend/app/controllers/api/v1/countries_controller.rb
+++ backend/app/controllers/api/v1/countries_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::CountriesController < ApplicationController
   def index
     render json: Country.select(:id, :name).as_json
-    rescue StandardError => e
-      ErrorUtility.log_and_notify(e)
-      render json: { error: "Countryモデルの取得に失敗しました。" }, status: :internal_server_error
+  rescue StandardError => e
+    ErrorUtility.log_and_notify(e)
+    render json: { error: "Countryモデルの取得に失敗しました。" }, status: :internal_server_error
   end
 end
