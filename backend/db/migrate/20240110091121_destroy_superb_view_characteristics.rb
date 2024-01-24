b02dc590ee7e6ae0d2dbe536ff3260851d31f631
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-01-24 14:28:55 +0900

    過去のmigrationファイルの書き換えで矛盾が発生したmigrationファイルを削除

--- backend/db/migrate/20240110091121_destroy_superb_view_characteristics.rb
+++
@@ -1,5 +0,0 @@
-class DestroySuperbViewCharacteristics < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :superb_view_characteristics
-  end
-end
--- backend/db/migrate/20240110091348_destroy_superb_view_countries.rb
+++
@@ -1,5 +0,0 @@
-class DestroySuperbViewCountries < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :superb_view_countries
-  end
-end
