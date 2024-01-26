3fa28d568fa66043d7435d576f828db7bc1db79b
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-01-26 09:01:15 +0900

    world_views_spec.rbを修正

--- backend/spec/requests/api/v1/world_views_spec.rb
+++ backend/spec/requests/api/v1/world_views_spec.rb
@@ -4,142 +4,126 @@ RSpec.describe "Api::V1::WorldViews", type: :request do
   describe "GET api/v1/world_views/search" do
     describe "world_view_filterメソッド内のスコープのテスト" do
       describe "filter_by_category_nameスコープのテスト" do
-        let!(:matera_cave_dwellings) { create(:world_view, name: "マテーラの洞窟住居") }
-        let!(:civita_di_bagnoregio) { create(:world_view, name: "チヴィタディバニョレージョ") }
-        let!(:machu_picchu) { create(:world_view, name: "マチュピチュ") }
-        let!(:category_city) { create(:category, name: "都市") }
-        let!(:category_cave) { create(:category, name: "洞窟") }
-        before do
-          create(:world_view_category, world_view: matera_cave_dwellings, category: category_cave)
-          create(:world_view_category, world_view: civita_di_bagnoregio, category: category_city)
-          create(:world_view_category, world_view: machu_picchu, category: category_city)
-        end
-
-        it "paramsで渡されたcategory_namesに一致するカテゴリー名を持つWorldViewを返すこと" do
+        it "paramsのcategory_namesの配列に含まれる名前のCategoryモデルと関連付けられたレコードを返すこと" do
+          world_view1 = create(:world_view)
+          world_view2 = create(:world_view)
+          category_cave = create(:category, name: "洞窟")
+          create(:world_view_category, world_view: world_view1, category: category_cave)
+          create(:world_view_category, world_view: world_view2, category: category_cave)
           get api_v1_world_views_search_path, params: {
-            category_names: [category_city.name]
+            category_names: ["洞窟"]
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include civita_di_bagnoregio.id.to_s
-          expect(response.body).to include machu_picchu.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(world_view1.id, world_view2.id)
         end
 
-        it "返されるWorldViewが重複しないこと" do
+        it "返されるレコードが重複しないこと" do
           duplicated_world_view = create(:world_view)
-          create(:world_view_category, world_view: duplicated_world_view, category: category_city)
-          create(:world_view_category, world_view: duplicated_world_view, category: category_cave)
-
+          category1 = create(:category)
+          category2 = create(:category)
+          create(:world_view_category, world_view: duplicated_world_view, category: category1)
+          create(:world_view_category, world_view: duplicated_world_view, category: category2)
           get api_v1_world_views_search_path, params: {
-            category_names: [category_city.name, category_cave.name]
+            category_names: [category1.name, category2.name]
           }
-          json = JSON.parse(response.body)
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(json).to eq json.uniq
+          expect(json_response).to eq json_response.uniq
         end
       end
 
       describe "filter_by_country_nameスコープのテスト" do
-        let!(:matera_cave_dwellings) { create(:world_view, name: "マテーラの洞窟住居") }
-        let!(:civita_di_bagnoregio) { create(:world_view, name: "チヴィタディバニョレージョ") }
-        let!(:machu_picchu) { create(:world_view, name: "マチュピチュ") }
-        let!(:country_italy) { create(:country, name: "イタリア") }
-        let!(:country_peru) { create(:country, name: "ペルー") }
-        before do
-          create(:world_view_country, world_view: matera_cave_dwellings, country: country_italy)
-          create(:world_view_country, world_view: civita_di_bagnoregio, country: country_italy)
-          create(:world_view_country, world_view: machu_picchu, country: country_peru)
-        end
-
-        it "paramsで渡されたcountry_namesに一致する国名を持つWorldViewを返すこと" do
+        it "paramsのcountry_namesの配列に含まれる名前のCountryモデルと関連付けられたレコードを返すこと" do
+          world_view1 = create(:world_view)
+          world_view2 = create(:world_view)
+          country_italy = create(:country, name: "イタリア")
+          create(:world_view_country, world_view: world_view1, country: country_italy)
+          create(:world_view_country, world_view: world_view2, country: country_italy)
           get api_v1_world_views_search_path, params: {
-            country_names: [country_italy.name]
+            country_names: ["イタリア"]
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include matera_cave_dwellings.id.to_s
-          expect(response.body).to include civita_di_bagnoregio.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(world_view1.id, world_view2.id)
         end
 
-        it "返されるWorldViewが重複しないこと" do
+        it "返されるレコードが重複しないこと" do
           duplicated_world_view = create(:world_view)
-          create(:world_view_country, world_view: duplicated_world_view, country: country_italy)
-          create(:world_view_country, world_view: duplicated_world_view, country: country_peru)
-
+          country1 = create(:country)
+          country2 = create(:country)
+          create(:world_view_country, world_view: duplicated_world_view, country: country1)
+          create(:world_view_country, world_view: duplicated_world_view, country: country2)
           get api_v1_world_views_search_path, params: {
-            country_names: [country_italy.name, country_peru.name]
+            country_names: [country1.name, country2.name]
           }
-          json = JSON.parse(response.body)
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(json).to eq json.uniq
+          expect(json_response).to eq json_response.uniq
         end
       end
 
       describe "filter_by_characteristic_nameスコープのテスト" do
-        let!(:matera_cave_dwellings) { create(:world_view, name: "マテーラの洞窟住居") }
-        let!(:civita_di_bagnoregio) { create(:world_view, name: "チヴィタディバニョレージョ") }
-        let!(:machu_picchu) { create(:world_view, name: "マチュピチュ") }
-        let!(:characteristic_historic) { create(:characteristic, name: "歴史・文化的") }
-        let!(:characteristic_fantasy) { create(:characteristic, name: "幻想・神秘的") }
-        before do
-          create(:world_view_characteristic, world_view: matera_cave_dwellings, characteristic: characteristic_fantasy)
-          create(:world_view_characteristic, world_view: civita_di_bagnoregio, characteristic: characteristic_historic)
-          create(:world_view_characteristic, world_view: machu_picchu, characteristic: characteristic_historic)
-        end
-
-        it "paramsで渡されたcharacteristic_namesに一致する属性名を持つWorldViewを返すこと" do
+        it "paramsのcharacteristic_namesの配列に含まれる名前のCharacteristicモデルと関連付けられたレコードを返すこと" do
+          world_view1 = create(:world_view)
+          world_view2 = create(:world_view)
+          characteristic_fantasy = create(:characteristic, name: "幻想・神秘的")
+          create(:world_view_characteristic, world_view: world_view1, characteristic: characteristic_fantasy)
+          create(:world_view_characteristic, world_view: world_view2, characteristic: characteristic_fantasy)
           get api_v1_world_views_search_path, params: {
-            characteristic_names: [characteristic_historic.name]
+            characteristic_names: ["幻想・神秘的"]
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include civita_di_bagnoregio.id.to_s
-          expect(response.body).to include machu_picchu.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(world_view1.id, world_view2.id)
         end
 
-        it "返されるWorldViewが重複しないこと" do
+        it "返されるレコードが重複しないこと" do
           duplicated_world_view = create(:world_view)
-          create(:world_view_characteristic, world_view: duplicated_world_view, characteristic: characteristic_historic)
-          create(:world_view_characteristic, world_view: duplicated_world_view, characteristic: characteristic_fantasy)
-
+          characteristic1 = create(:characteristic)
+          characteristic2 = create(:characteristic)
+          create(:world_view_characteristic, world_view: duplicated_world_view, characteristic: characteristic1)
+          create(:world_view_characteristic, world_view: duplicated_world_view, characteristic: characteristic2)
           get api_v1_world_views_search_path, params: {
-            characteristic_names: [characteristic_historic.name, characteristic_fantasy.name]
+            characteristic_names: [characteristic1.name, characteristic2.name]
           }
-          json = JSON.parse(response.body)
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(json).to eq json.uniq
+          expect(json_response).to eq json_response.uniq
         end
       end
 
       describe "filter_by_country_risk_levelスコープのテスト" do
-        let!(:matera_cave_dwellings) { create(:world_view, name: "マテーラの洞窟住居") }
-        let!(:civita_di_bagnoregio) { create(:world_view, name: "チヴィタディバニョレージョ") }
-        let!(:machu_picchu) { create(:world_view, name: "マチュピチュ") }
-        let!(:country_italy) { create(:country, name: "イタリア", risk_level: 0) }
-        let!(:country_peru) { create(:country, name: "ペルー", risk_level: 1) }
-        before do
-          create(:world_view_country, world_view: matera_cave_dwellings, country: country_italy)
-          create(:world_view_country, world_view: civita_di_bagnoregio, country: country_italy)
-          create(:world_view_country, world_view: machu_picchu, country: country_peru)
-        end
-
-        it "paramsで渡されたrisk_levelsに一致する国名を持つWorldViewを返すこと" do
+        it "paramsのrisk_levelsの配列に含まれるrisk_levelのCountryモデルと関連付けられたレコードを返すこと" do
+          world_view1 = create(:world_view)
+          world_view2 = create(:world_view)
+          country1 = create(:country, risk_level: 1)
+          create(:world_view_country, world_view: world_view1, country: country1)
+          create(:world_view_country, world_view: world_view2, country: country1)
           get api_v1_world_views_search_path, params: {
-            risk_levels: ["0"]
+            risk_levels: ["1"]
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include matera_cave_dwellings.id.to_s
-          expect(response.body).to include civita_di_bagnoregio.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(world_view1.id, world_view2.id)
         end
 
-        it "返されるWorldViewが重複しないこと" do
+        it "返されるレコードが重複しないこと" do
           duplicated_world_view = create(:world_view)
-          create(:world_view_country, world_view: duplicated_world_view, country: country_italy)
-          create(:world_view_country, world_view: duplicated_world_view, country: country_peru)
-
+          country1 = create(:country, risk_level: 1)
+          country2 = create(:country, risk_level: 2)
+          create(:world_view_country, world_view: duplicated_world_view, country: country1)
+          create(:world_view_country, world_view: duplicated_world_view, country: country2)
           get api_v1_world_views_search_path, params: {
-            risk_levels: ["0", "1"]
+            risk_levels: ["1", "2"]
           }
-          json = JSON.parse(response.body)
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(json).to eq json.uniq
+          expect(json_response).to eq json_response.uniq
         end
       end
 
@@ -155,31 +139,32 @@ RSpec.describe "Api::V1::WorldViews", type: :request do
           create(:world_view_country, world_view: machu_picchu, country: country_peru)
         end
 
-        it "paramsで受け取ったkeywordに部分一致する名前を持つWorldViewを返すこと" do
+        it "paramsのkeywordに部分一致する名前のレコードを返すこと" do
           get api_v1_world_views_search_path, params: {
             keyword: "マ"
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include matera_cave_dwellings.id.to_s
-          expect(response.body).to include machu_picchu.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(matera_cave_dwellings.id, machu_picchu.id)
         end
 
-        it "paramsで受け取ったkeywordに部分一致する国名を持つWorldViewを返すこと" do
+        it "paramsのkeywordに部分一致するCountryモデルと関連づけられたレコードを返すこと" do
           get api_v1_world_views_search_path, params: {
             keyword: "イタリア"
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include matera_cave_dwellings.id.to_s
-          expect(response.body).to include civita_di_bagnoregio.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(matera_cave_dwellings.id, civita_di_bagnoregio.id)
         end
 
-        it "返されるWorldViewが重複しないこと" do
-          duplicate_world_view = create(:world_view, name: "重複する絶景")
+        it "返されるレコードが重複しないこと" do
+          duplicate_world_view = create(:world_view, name: "重複するレコード")
           create(:world_view_country, world_view: duplicate_world_view, country: country_italy)
           create(:world_view_country, world_view: duplicate_world_view, country: country_peru)
-
           get api_v1_world_views_search_path, params: {
-            keyword: "重複する絶景"
+            keyword: "重複するレコード"
           }
           json = JSON.parse(response.body)
           expect(response).to have_http_status(200)
@@ -188,23 +173,22 @@ RSpec.describe "Api::V1::WorldViews", type: :request do
       end
 
       describe "filter_by_monthのスコープのテスト" do
-        it "paramsで受け取ったmonthsの月をbest_seasonに持つWorldViewを返すこと" do
+        it "paramsのmonthsの配列に含まれる月をbest_seasonに持つCountryモデルと関連づけられたレコードを返すこと" do
           world_view1 = create(:world_view, best_season: "12月〜2月")
           world_view2 = create(:world_view, best_season: "3月〜5月 or 9月〜11月")
           world_view3 = create(:world_view, best_season: "6月〜8月")
-
           get api_v1_world_views_search_path, params: {
             months: ["1月", "3月", "7月"]
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include world_view1.id.to_s
-          expect(response.body).to include world_view2.id.to_s
-          expect(response.body).to include world_view3.id.to_s
+          expect(json_response.length).to eq(3)
+          expect(json_response.map { |view| view["id"] }).to include(world_view1.id, world_view2.id, world_view3.id)
         end
       end
 
       describe "filter_by_country_bmiスコープのテスト" do
-        it "paramsで受け取ったbmi_rangesの範囲に含まれるbmiカラムを持つCountryモデルと関連しているWorldViewを返すこと" do
+        it "paramsのbmi_rangesの範囲に含まれるbmiカラムを持つCountryモデルと関連づけられたレコードを返すこと" do
           world_view1 = create(:world_view)
           world_view2 = create(:world_view)
           world_view3 = create(:world_view)
@@ -214,22 +198,22 @@ RSpec.describe "Api::V1::WorldViews", type: :request do
           get api_v1_world_views_search_path, params: {
             bmi_ranges: ["20%〜30%", "〜-40%"]
           }
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(response.body).to include world_view1.id.to_s
-          expect(response.body).not_to include world_view2.id.to_s
-          expect(response.body).to include world_view3.id.to_s
+          expect(json_response.length).to eq(2)
+          expect(json_response.map { |view| view["id"] }).to include(world_view1.id, world_view3.id)
         end
 
-        it "返されるWorldViewが重複しないこと" do
+        it "返されるレコードが重複しないこと" do
           duplicated_world_view = create(:world_view)
           create(:world_view_country, world_view: duplicated_world_view, country: create(:country, bmi: -10.2))
           create(:world_view_country, world_view: duplicated_world_view, country: create(:country, bmi: 1.2))
           get api_v1_world_views_search_path, params: {
             bmi_ranges: ["-20%〜-10%", "0〜10%"]
           }
-          json = JSON.parse(response.body)
+          json_response = JSON.parse(response.body)
           expect(response).to have_http_status(200)
-          expect(json).to eq json.uniq
+          expect(json_response).to eq json_response.uniq
         end
       end
     end
