ded696db663bdefb6ecd5b140e5e28d4f1f9684a
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-01-29 15:13:25 +0900

    AuthContext.tsを削除

--- frontend/app/src/providers/useAuthProvider.tsx
+++ frontend/app/src/providers/useAuthProvider.tsx
@@ -1,17 +1,22 @@
-import AuthContext from "contexts/AuthContext";
 import { User } from "features/auth/types/user";
-import { FC, memo, ReactNode, useContext, useMemo, useState } from "react";
+import { createContext, FC, memo, ReactNode, useContext, useMemo, useState } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
-export const useAuth = () => {
-  const authContext = useContext(AuthContext);
-  return useMemo(() => authContext, [authContext]);
-};
+const AuthContext = createContext(
+  {} as {
+    loading: boolean;
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    isSignedIn: boolean;
+    setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>;
+    currentUser: User | undefined;
+    setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>;
+  }
+);
 
-export const AuthProvider: FC<Props> = memo(({ children }) => {
+const AuthProvider: FC<Props> = memo(({ children }) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
   const [currentUser, setCurrentUser] = useState<User | undefined>();
@@ -29,3 +34,10 @@ export const AuthProvider: FC<Props> = memo(({ children }) => {
   );
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 });
+
+const useAuth = () => {
+  const authContext = useContext(AuthContext);
+  return useMemo(() => authContext, [authContext]);
+};
+
+export { AuthProvider, useAuth };
