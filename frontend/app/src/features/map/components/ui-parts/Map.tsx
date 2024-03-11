514cfdb03cea555b3af1b54d33028561d9dee053
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-03-11 15:46:48 +0900

    mapにキーワード検索機能を追加

--- frontend/app/src/features/map/components/ui-parts/Map.tsx
+++ frontend/app/src/features/map/components/ui-parts/Map.tsx
@@ -1,11 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import defaultImg from "assets/default.png";
+import { AxiosResponse } from "axios";
 import ClickHandler from "features/map/components/ui-elements/ClickHandler";
 import ZoomDependentImageOverlay from "features/map/components/ui-elements/ZoomDependentImageOverlay";
 import WorldViewList from "features/map/components/ui-parts/WorldViewList";
-import getAllWorldViewsApi from "features/worldView/api/getAllWorldViewsApi";
+import useWorldViewApi from "features/worldView/api/useWorldViewApi";
+import FilterSearchBox from "features/worldView/components/ui-elements/FilterSearchBox";
 import { WorldView } from "features/worldView/types/api/worldView";
 import useGetModel from "hooks/api/useGetModel";
+import useDebounce from "hooks/useDebounce";
 import "leaflet/dist/leaflet.css";
 import { useWorldViewListContext } from "providers/WorldViewListProvider";
 import { useEffect, useState } from "react";
@@ -13,34 +16,58 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 const Map = () => {
   const { state, dispatch } = useWorldViewListContext();
-  const { worldViews } = state;
+  const { worldViews, keyword, isSkipSearchApi, shouldDebounce } = state;
+  const { handleGetModel } = useGetModel();
+  const { handleDebounceWithArg } = useDebounce(1500);
+  const { searchWorldViewApi } = useWorldViewApi();
   const [clickedViews, setClickedViews] = useState<
     Pick<WorldView, "id" | "name" | "imgUrl" | "countries" | "latitude" | "longitude">[] | undefined
   >(undefined);
 
-  const loadingGetWorldViewDispatch = (payload: boolean) => {
-    dispatch({ type: "SET_LOADING_GET_WORLDVIEWS", payload });
+  const loadingSearchWorldViewDispatch = (payload: boolean) => {
+    dispatch({ type: "SET_LOADING_SEARCH_WORLDVIEWS", payload });
   };
   const worldViewDispatch = (responseData: WorldView[]) => {
     dispatch({ type: "SET_WORLD_VIEWS", payload: responseData });
   };
-  const { handleGetModel } = useGetModel();
-
   useEffect(() => {
-    handleGetModel<WorldView>({
-      loadingGetModelDispatch: loadingGetWorldViewDispatch,
-      modelDispatch: worldViewDispatch,
-      getModelApi: getAllWorldViewsApi,
-    });
-  }, []);
+    if (!isSkipSearchApi) {
+      if (shouldDebounce) {
+        handleDebounceWithArg<{
+          loadingGetModelDispatch: (payload: boolean) => void;
+          modelDispatch: (responseData: WorldView[]) => void;
+          getModelApi: () => Promise<AxiosResponse<WorldView[]>>;
+        }>({
+          fn: handleGetModel,
+          arg: {
+            loadingGetModelDispatch: loadingSearchWorldViewDispatch,
+            modelDispatch: worldViewDispatch,
+            getModelApi: searchWorldViewApi,
+          },
+        });
+        dispatch({ type: "SET_SHOULD_DEBOUNCE", payload: false });
+      } else {
+        handleGetModel<WorldView>({
+          loadingGetModelDispatch: loadingSearchWorldViewDispatch,
+          modelDispatch: worldViewDispatch,
+          getModelApi: searchWorldViewApi,
+        });
+      }
+    } else {
+      dispatch({ type: "SET_IS_SKIP_SEARCH_API", payload: false });
+    }
+  }, [keyword]);
 
   return (
-    <Box bg="white">
+    <Box bg="white" style={{ position: "relative" }}>
+      <Box style={{ position: "absolute", zIndex: 1, left: "33%", width: "30%" }} mt="3">
+        <FilterSearchBox />
+      </Box>
       <MapContainer
         center={[30, 0]}
         zoom={2}
         scrollWheelZoom={false}
-        style={{ height: "65vh", width: "100%" }}
+        style={{ height: "65vh", width: "100%", position: "relative", zIndex: 0 }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- frontend/app/src/features/map/test/components/ui-parts/Map.test.tsx
+++ frontend/app/src/features/map/test/components/ui-parts/Map.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Map from "features/map/components/ui-parts/Map";
+import { useWorldViewListContext as mockUseWorldViewListContext } from "providers/WorldViewListProvider";
+import { act } from "react-dom/test-utils";
 
 const mockNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockNavigate,
+}));
+
+jest.mock("providers/WorldViewListProvider", () => ({
+  useWorldViewListContext: jest.fn(),
 }));
 
 const mockDispatch = jest.fn();
@@ -22,24 +28,120 @@ const mockWorldViews = Array.from({ length: 3 }, (_, index) => ({
   longitude: index + 1,
 }));
 
-jest.mock("providers/WorldViewListProvider", () => ({
-  useWorldViewListContext: () => ({
-    dispatch: mockDispatch,
-    state: { worldViews: mockWorldViews },
-  }),
+const mockContextValue = {
+  dispatch: mockDispatch,
+  state: {
+    worldViews: mockWorldViews,
+    categoryCheckItems: [],
+    countryCheckItems: [],
+    characteristicCheckItems: [],
+    keyword: "",
+    isSkipSearchApi: false,
+    shouldDebounce: false,
+  },
+};
+
+const mockContextValueIsSkipSearchApi = {
+  ...mockContextValue,
+  state: {
+    ...mockContextValue.state,
+    isSkipSearchApi: true,
+  },
+};
+
+const mockContextValueShouldDebounce = {
+  ...mockContextValue,
+  state: {
+    ...mockContextValue.state,
+    shouldDebounce: true,
+  },
+};
+
+const mockSearchWorldViewApi = jest.fn();
+jest.mock("features/worldView/api/useWorldViewApi", () => ({
+  __esModule: true,
+  default: () => ({ searchWorldViewApi: mockSearchWorldViewApi }),
 }));
 
+test("isSkipSearchApiがtrueの場合、falseに更新されること", () => {
+  (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValueIsSkipSearchApi);
+  render(<Map />);
+  expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_IS_SKIP_SEARCH_API", payload: false });
+});
+
+test("shouldDebounceがtrueの場合、flaseに更新されること", () => {
+  (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValueShouldDebounce);
+  render(<Map />);
+  expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_SHOULD_DEBOUNCE", payload: false });
+});
+
+describe("handleGetModel関数のテスト", () => {
+  test("初回レンダリング時にhandleGetModel関数が実行されること", async () => {
+    (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValue);
+
+    // handleGetModel関数のモック化
+    const spyOnUseHandleGetModel = jest.spyOn(
+      jest.requireActual("hooks/api/useGetModel"),
+      "default"
+    );
+    const mockHandleGetModel = jest.fn();
+    spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
+
+    await act(async () => {
+      render(<Map />);
+    });
+    expect(mockHandleGetModel).toHaveBeenCalledWith({
+      loadingGetModelDispatch: expect.any(Function),
+      modelDispatch: expect.any(Function),
+      getModelApi: mockSearchWorldViewApi,
+    });
+    spyOnUseHandleGetModel.mockRestore();
+  });
+
+  test("初回レンダリング時にhandleGetModel関数内でworldViewDispatch関数が呼び出されること", async () => {
+    (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValue);
+    mockSearchWorldViewApi.mockReturnValue({ data: [{ id: 1, name: "name" }] });
+
+    await act(async () => {
+      render(<Map />);
+    });
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: "SET_WORLD_VIEWS",
+      payload: [{ id: 1, name: "name" }],
+    });
+  });
+
+  test("初回レンダリング時にhandleGetModel関数内でloadingSearchWorldViewDispatch関数が呼び出されること", async () => {
+    (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValue);
+
+    await act(async () => {
+      render(<Map />);
+    });
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: "SET_LOADING_SEARCH_WORLDVIEWS",
+      payload: true,
+    });
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: "SET_LOADING_SEARCH_WORLDVIEWS",
+      payload: false,
+    });
+  });
+});
+
 test("ズームボタンが表示されていること", () => {
+  (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValue);
   render(<Map />);
   expect(screen.getByRole("button", { name: "Zoom in" }));
 });
 
 test("ズームアウトボタンが表示されていること", () => {
+  (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValue);
   render(<Map />);
   expect(screen.getByRole("button", { name: "Zoom out" }));
 });
 
 test("絶景画像が地図上に表示されていること", () => {
+  (mockUseWorldViewListContext as jest.Mock).mockReturnValue(mockContextValue);
   render(<Map />);
   const WorldViewImgs = screen.getAllByRole("img", { name: "絶景画像" });
   expect(WorldViewImgs.length).toBe(3);
