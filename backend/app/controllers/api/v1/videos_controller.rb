bf51601db58c7b2acf6ac844a01a17403733f9b2
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-03-26 17:35:42 +0900

    Videoモデルの並び替えのscopeを修正

--- backend/app/controllers/api/v1/videos_controller.rb
+++ backend/app/controllers/api/v1/videos_controller.rb
@@ -1,33 +1,18 @@
 class Api::V1::VideosController < ApplicationController
   def search
-    sorted_video = video_sort(Video)
-    filtered_video = video_filter(sorted_video)
-    render json: filtered_video.preload(:genres, :world_views, world_views: [:countries])
-                               .as_json(except: %i[created_at updated_at],
-                                        include: [{ genres: { only: %i[id name] } },
-                                                  { world_views: { only: %i[id name img_url latitude longitude],
-                                                                   include: { countries: { only: [:id, :name] } } } }])
+    filtered_video = video_filter(Video)
+    sorted_video = filtered_video.order_by(video_params[:sort_criteria])
+    render json: sorted_video.preload(:genres, :world_views, world_views: [:countries])
+                             .as_json(except: %i[created_at updated_at],
+                                      include: [{ genres: { only: %i[id name] } },
+                                                { world_views: { only: %i[id name img_url latitude longitude],
+                                                                 include: { countries: { only: [:id, :name] } } } }])
   end
 
   private
 
   def video_params
     params.permit(:sort_criteria, :keyword, genre_labels: [], vote_average_range: [])
-  end
-
-  def video_sort(video)
-    case video_params[:sort_criteria]
-    when "popularity"
-      video.sort_by_popularity
-    when "voteAverage"
-      video.sort_by_vote_average
-    when "releaseDate"
-      video.sort_by_release_date
-    when ""
-      Video.all
-    else
-      raise ArgumentError, "Invalid sort criteria: #{video_params[:sort_criteria]}"
-    end
   end
 
   def video_filter(video)
--- backend/spec/requests/api/v1/videos_spec.rb
+++ backend/spec/requests/api/v1/videos_spec.rb
@@ -2,83 +2,6 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Videos", type: :request do
   describe "GET /search" do
-    describe "video_sortメソッドのテスト" do
-      context "sort_criteriaがpopularityの場合" do
-        let!(:video1) { create(:video, popularity: 1) }
-        let!(:video2) { create(:video, popularity: 2) }
-        let!(:video3) { create(:video, popularity: 3) }
-
-        before do
-          get api_v1_videos_search_path, params: { sort_criteria: "popularity" }
-        end
-
-        it "popularityが高い順にレコードが取得されること" do
-          expect(response).to have_http_status(200)
-          json = JSON.parse(response.body)
-          expect(json.pluck("id")).to eq [video3, video2, video1].pluck(:id)
-        end
-      end
-
-      context "sort_criteriaがvoteAverageの場合" do
-        let!(:video1) { create(:video, vote_average: 1) }
-        let!(:video2) { create(:video, vote_average: 2) }
-        let!(:video3) { create(:video, vote_average: 3) }
-
-        before do
-          get api_v1_videos_search_path, params: { sort_criteria: "voteAverage" }
-        end
-
-        it "vote_averageが高い順にレコードが取得されること" do
-          expect(response).to have_http_status(200)
-          json = JSON.parse(response.body)
-          expect(json.pluck("id")).to eq [video3, video2, video1].pluck(:id)
-        end
-      end
-
-      context "sort_criteriaがreleaseDateの場合" do
-        let!(:video1) { create(:video, release_date: 3.days.ago) }
-        let!(:video2) { create(:video, release_date: 2.days.ago) }
-        let!(:video3) { create(:video, release_date: 1.day.ago) }
-
-        before do
-          get api_v1_videos_search_path, params: { sort_criteria: "releaseDate" }
-        end
-
-        it "release_dateが遅い順にレコードが取得されること" do
-          expect(response).to have_http_status(200)
-          json = JSON.parse(response.body)
-          expect(json.pluck("id")).to eq [video3, video2, video1].pluck(:id)
-        end
-      end
-
-      context "sort_criteriaが空文字の場合" do
-        let!(:videos) { create_list(:video, 5) }
-
-        before do
-          get api_v1_videos_search_path, params: { sort_criteria: "" }
-        end
-
-        it "Videoレコードが全件取得できること" do
-          expect(response).to have_http_status(200)
-          json = JSON.parse(response.body)
-          expected_json = videos.as_json(except: %i[created_at updated_at],
-                                         include: [{ genres: { only: %i[id name] } },
-                                                   { world_views: { only: %i[id name img_url],
-                                                                    include: { countries: { only: [:id, :name] } } } }])
-          expect(json).to eq expected_json
-          expect(json.length).to eq 5
-        end
-      end
-
-      context "sort_criteriaが不正な場合" do
-        it "ArgumentErrorが発生すること" do
-          expect do
-            get api_v1_videos_search_path, params: { sort_criteria: "invalid_sort_criteria" }
-          end.to raise_error(ArgumentError, "Invalid sort criteria: invalid_sort_criteria")
-        end
-      end
-    end
-
     describe ".video_filter" do
       describe ".filter_by_genre" do
         let!(:video1) { create(:video) }
@@ -163,6 +86,81 @@ RSpec.describe "Api::V1::Videos", type: :request do
             expect(json_response.length).to eq(2)
             expect(json_response.pluck("id")).to include video1.id, video2.id
           end
+        end
+      end
+    end
+
+    describe ".order_by" do
+      context "sort_criteriaがpopularityの場合" do
+        let!(:video1) { create(:video, popularity: 1) }
+        let!(:video2) { create(:video, popularity: 2) }
+        let!(:video3) { create(:video, popularity: 3) }
+
+        before do
+          get api_v1_videos_search_path, params: { sort_criteria: "popularity" }
+        end
+
+        it "popularityが高い順にレコードが取得されること" do
+          expect(response).to have_http_status(200)
+          json = JSON.parse(response.body)
+          expect(json.pluck("id")).to eq [video3, video2, video1].pluck(:id)
+        end
+      end
+
+      context "sort_criteriaがvoteAverageの場合" do
+        let!(:video1) { create(:video, vote_average: 1) }
+        let!(:video2) { create(:video, vote_average: 2) }
+        let!(:video3) { create(:video, vote_average: 3) }
+
+        before do
+          get api_v1_videos_search_path, params: { sort_criteria: "voteAverage" }
+        end
+
+        it "vote_averageが高い順にレコードが取得されること" do
+          expect(response).to have_http_status(200)
+          json = JSON.parse(response.body)
+          expect(json.pluck("id")).to eq [video3, video2, video1].pluck(:id)
+        end
+      end
+
+      context "sort_criteriaがreleaseDateの場合" do
+        let!(:video1) { create(:video, release_date: 3.days.ago) }
+        let!(:video2) { create(:video, release_date: 2.days.ago) }
+        let!(:video3) { create(:video, release_date: 1.day.ago) }
+
+        before do
+          get api_v1_videos_search_path, params: { sort_criteria: "releaseDate" }
+        end
+
+        it "release_dateが遅い順にレコードが取得されること" do
+          expect(response).to have_http_status(200)
+          json = JSON.parse(response.body)
+          expect(json.pluck("id")).to eq [video3, video2, video1].pluck(:id)
+        end
+      end
+
+      context "sort_criteriaが空文字の場合" do
+        let!(:video1) { create(:video) }
+        let!(:video2) { create(:video) }
+        let!(:video3) { create(:video) }
+
+        before do
+          get api_v1_videos_search_path, params: { sort_criteria: "" }
+        end
+
+        it "レコードが処理されずに取得されること" do
+          expect(response).to have_http_status(200)
+          json_response = JSON.parse(response.body)
+          expect(json_response.pluck("id")).to include video1.id, video2.id, video3.id
+          expect(json_response.length).to eq 3
+        end
+      end
+
+      context "sort_criteriaが不正な場合" do
+        it "ArgumentErrorが発生すること" do
+          expect do
+            get api_v1_videos_search_path, params: { sort_criteria: "invalid_sort_criteria" }
+          end.to raise_error(ArgumentError, "Invalid sort criteria: invalid_sort_criteria")
         end
       end
     end
