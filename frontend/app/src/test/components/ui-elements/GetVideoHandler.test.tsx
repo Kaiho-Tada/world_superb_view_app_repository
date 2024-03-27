af7e1c5dc9b712d7b93b78be6e93c12f1066e4e5
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2024-03-27 16:55:05 +0900

    不要なdiscribeを削除

--- frontend/app/src/test/components/ui-elements/GetVideoHandler.test.tsx
+++ frontend/app/src/test/components/ui-elements/GetVideoHandler.test.tsx
@@ -80,9 +80,74 @@ jest.mock("features/video/api/videoApi", () => ({
   default: () => ({ searchVideoApi: mockSearchVideoApi }),
 }));
 
-describe("初回レンダリング時のテスト", () => {
-  test("isSkipSearchVideoがtrueの場合、falseに更新され、handleGetModel関数が実行されないこと", async () => {
-    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueSkipSearchVideo);
+test("isSkipSearchVideoがtrueの場合、falseに更新され、handleGetModel関数が実行されないこと", async () => {
+  (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueSkipSearchVideo);
+
+  // handleGetModel関数のモック化
+  const spyOnUseHandleGetModel = jest.spyOn(jest.requireActual("hooks/api/useGetModel"), "default");
+  const mockHandleGetModel = jest.fn();
+  spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
+
+  await act(async () => {
+    render(<GetVideoHandler />);
+  });
+  expect(mockHandleGetModel).not.toHaveBeenCalled();
+  expect(mockDispatch).toHaveBeenCalledWith({
+    type: "SET_IS_SKIP_SEARCH_VIDEO",
+    payload: false,
+  });
+  spyOnUseHandleGetModel.mockRestore();
+});
+
+test("isVisitedDetailPageがtrueの場合、falseに更新され、handleGetModel関数が実行されないこと", async () => {
+  (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueVisitedDetailPage);
+
+  // handleGetModel関数のモック化
+  const spyOnUseHandleGetModel = jest.spyOn(jest.requireActual("hooks/api/useGetModel"), "default");
+  const mockHandleGetModel = jest.fn();
+  spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
+
+  await act(async () => {
+    render(<GetVideoHandler />);
+  });
+  expect(mockHandleGetModel).not.toHaveBeenCalled();
+  expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_IS_VISIT_DETAIL_PAGE", payload: false });
+  spyOnUseHandleGetModel.mockRestore();
+});
+
+test("shouldDebounceがtrueの場合、falseに更新され、handleDebounce関数が実行されること", async () => {
+  (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueShouldDebounce);
+
+  // handleGetModel関数のモック化
+  const spyOnUseHandleGetModel = jest.spyOn(jest.requireActual("hooks/api/useGetModel"), "default");
+  const mockHandleGetModel = jest.fn();
+  spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
+
+  await act(async () => {
+    render(<GetVideoHandler />);
+  });
+  expect(mockHandleDebounce).toHaveBeenCalledWith(expect.any(Function));
+  expect(mockHandleDebounce).toHaveBeenCalledTimes(1);
+  expect(mockDispatch).toHaveBeenCalledWith({
+    type: "SET_SHOULD_DEBOUNCE",
+    payload: false,
+  });
+  spyOnUseHandleGetModel.mockRestore();
+});
+
+test("ページネーションが1ページ目ではない場合はcurrentPageが1にitemsOffsetが0に更新されること", async () => {
+  (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueCurrentPage2);
+
+  await act(async () => {
+    render(<GetVideoHandler />);
+  });
+  expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_CURRENT_PAGE", payload: 1 });
+  expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_ITEMS_OFFSET", payload: 0 });
+});
+
+describe("handleGetModel関数のテスト", () => {
+  test("handleGetModel関数が実行されること", async () => {
+    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValue);
 
     // handleGetModel関数のモック化
     const spyOnUseHandleGetModel = jest.spyOn(
@@ -95,117 +160,41 @@ describe("初回レンダリング時のテスト", () => {
     await act(async () => {
       render(<GetVideoHandler />);
     });
-    expect(mockHandleGetModel).not.toHaveBeenCalled();
+    expect(mockHandleGetModel).toHaveBeenCalledWith({
+      loadingGetModelDispatch: expect.any(Function),
+      modelDispatch: expect.any(Function),
+      getModelApi: mockSearchVideoApi,
+    });
+    spyOnUseHandleGetModel.mockRestore();
+  });
+
+  test("handleGetModel関数内でsetVideoDispatch関数が呼び出されること", async () => {
+    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValue);
+
+    mockSearchVideoApi.mockReturnValue({ data: [{ id: 1, name: "name" }] });
+
+    await act(async () => {
+      render(<GetVideoHandler />);
+    });
     expect(mockDispatch).toHaveBeenCalledWith({
-      type: "SET_IS_SKIP_SEARCH_VIDEO",
-      payload: false,
+      type: "SET_VIDEOS",
+      payload: [{ id: 1, name: "name" }],
     });
-    spyOnUseHandleGetModel.mockRestore();
   });
 
-  test("isVisitedDetailPageがtrueの場合、falseに更新され、handleGetModel関数が実行されないこと", async () => {
-    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueVisitedDetailPage);
-
-    // handleGetModel関数のモック化
-    const spyOnUseHandleGetModel = jest.spyOn(
-      jest.requireActual("hooks/api/useGetModel"),
-      "default"
-    );
-    const mockHandleGetModel = jest.fn();
-    spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
+  test("handleGetModel関数内でsetLoadingSearchVideoDispatch関数が呼び出されること", async () => {
+    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValue);
 
     await act(async () => {
       render(<GetVideoHandler />);
     });
-    expect(mockHandleGetModel).not.toHaveBeenCalled();
-    expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_IS_VISIT_DETAIL_PAGE", payload: false });
-    spyOnUseHandleGetModel.mockRestore();
-  });
-
-  test("shouldDebounceがtrueの場合、falseに更新され、handleDebounce関数が実行されること", async () => {
-    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueShouldDebounce);
-
-    // handleGetModel関数のモック化
-    const spyOnUseHandleGetModel = jest.spyOn(
-      jest.requireActual("hooks/api/useGetModel"),
-      "default"
-    );
-    const mockHandleGetModel = jest.fn();
-    spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
-
-    await act(async () => {
-      render(<GetVideoHandler />);
-    });
-    expect(mockHandleDebounce).toHaveBeenCalledWith(expect.any(Function));
-    expect(mockHandleDebounce).toHaveBeenCalledTimes(1);
     expect(mockDispatch).toHaveBeenCalledWith({
-      type: "SET_SHOULD_DEBOUNCE",
+      type: "SET_LOADING_SEARCH_VIDEOS",
+      payload: true,
+    });
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: "SET_LOADING_SEARCH_VIDEOS",
       payload: false,
-    });
-    spyOnUseHandleGetModel.mockRestore();
-  });
-
-  test("ページネーションが1ページ目ではない場合はcurrentPageが1にitemsOffsetが0に更新されること", async () => {
-    (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValueCurrentPage2);
-
-    await act(async () => {
-      render(<GetVideoHandler />);
-    });
-    expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_CURRENT_PAGE", payload: 1 });
-    expect(mockDispatch).toHaveBeenCalledWith({ type: "SET_ITEMS_OFFSET", payload: 0 });
-  });
-
-  describe("handleGetModel関数のテスト", () => {
-    test("handleGetModel関数が実行されること", async () => {
-      (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValue);
-
-      // handleGetModel関数のモック化
-      const spyOnUseHandleGetModel = jest.spyOn(
-        jest.requireActual("hooks/api/useGetModel"),
-        "default"
-      );
-      const mockHandleGetModel = jest.fn();
-      spyOnUseHandleGetModel.mockReturnValue({ handleGetModel: mockHandleGetModel });
-
-      await act(async () => {
-        render(<GetVideoHandler />);
-      });
-      expect(mockHandleGetModel).toHaveBeenCalledWith({
-        loadingGetModelDispatch: expect.any(Function),
-        modelDispatch: expect.any(Function),
-        getModelApi: mockSearchVideoApi,
-      });
-      spyOnUseHandleGetModel.mockRestore();
-    });
-
-    test("handleGetModel関数内でsetVideoDispatch関数が呼び出されること", async () => {
-      (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValue);
-
-      mockSearchVideoApi.mockReturnValue({ data: [{ id: 1, name: "name" }] });
-
-      await act(async () => {
-        render(<GetVideoHandler />);
-      });
-      expect(mockDispatch).toHaveBeenCalledWith({
-        type: "SET_VIDEOS",
-        payload: [{ id: 1, name: "name" }],
-      });
-    });
-
-    test("handleGetModel関数内でsetLoadingSearchVideoDispatch関数が呼び出されること", async () => {
-      (mockUseVideoListContext as jest.Mock).mockReturnValue(mockContextValue);
-
-      await act(async () => {
-        render(<GetVideoHandler />);
-      });
-      expect(mockDispatch).toHaveBeenCalledWith({
-        type: "SET_LOADING_SEARCH_VIDEOS",
-        payload: true,
-      });
-      expect(mockDispatch).toHaveBeenCalledWith({
-        type: "SET_LOADING_SEARCH_VIDEOS",
-        payload: false,
-      });
     });
   });
 });
